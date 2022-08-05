e867ab16ae18fb07c68243c9196c85d35af47238
Eric Song <ericksong91@gmail.com>
2022-08-05 15:54:44 -0400

    done with assignment

--- demo.js
+++ demo.js
@@ -41,7 +41,7 @@ function likeCallback(e) {
 for (const glyph of articleHearts) {
   glyph.addEventListener("click", likeCallback);
 }
-
+  
 // STEP 4: 
 
 // When all the STEPs' code changes have been complete, you'll be able to see a
